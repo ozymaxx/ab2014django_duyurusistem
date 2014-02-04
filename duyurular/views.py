727daea6bf53b371b5af5904deb271983134c879
mustafaiman <mustafaiman@gmail.com>
2014-02-04 12:25:43 +0200

    template

--- duyurular/views.py
+++ duyurular/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
 
 def create_post( request):
+    pass
 	
 def signup( request):
-	
+	pass
+
 def post_search( request):
+    return render_to_response("posts.html")
 	
 def single_post( request, post_id):
+    pass
 	
 def daily_post( request, year, month, day):
+    pass
 	
 def login( request):
+    pass
